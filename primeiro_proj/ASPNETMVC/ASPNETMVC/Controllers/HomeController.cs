55403cfe164a1737e7015812e20149bb464b7f76
Stenio das Neves Silva <stenionevessilva@gmail.com>
2018-07-21 14:41:01 -0300

    Primeiro projeto em C# padrão MVC

---
+++ primeiro_proj/ASPNETMVC/ASPNETMVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNETMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        [HttpPost]
+        [HttpGet]
+        public IActionResult Index(HomeModel formulario)
+        {
+           
+            CarregarListaDados();
+            return View();
+        }
+
+        public void CarregarListaDados()
+        {
+            HomeModel objHomeModel = new HomeModel();
+            ViewBag.Lista = objHomeModel.GetAll()
+                ;
+            
+        }
+    }
+}
---
+++ primeiro_proj/ASPNETMVC/ASPNETMVC/Models/HomeModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETMVC.Models
+{
+    public class HomeModel
+    {
+        public int Id { set; get; }
+        public string Nome { set; get; }
+
+        public List<HomeModel> GetAll()
+        {
+            HomeModel item;
+            List<HomeModel> lista = new List<HomeModel>();
+            item = new HomeModel();
+            item.Id = 1;
+            item.Nome = "Stenio Neves";
+            lista.Add(item);
+
+            item = new HomeModel();
+            item.Id = 2;
+            item.Nome = "Silva";
+            lista.Add(item);
+            
+        
+            return lista;
+        }
+
+    }
+}
