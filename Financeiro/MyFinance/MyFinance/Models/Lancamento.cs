dce090511513ce74017f5b4a2808cc86035da8b1
Stenio das Neves Silva <stenionevessilva@gmail.com>
2019-08-05 17:24:12 -0300

    Criação da classe Lancamento para mais detalhes das transação como itens associado e valores e local da compra

---
+++ Financeiro/MyFinance/MyFinance/Models/Lancamento.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using MyFinance.Util;
+
+
+namespace MyFinance.Models
+{
+    public class Lancamento
+    {
+
+        private int Id { get; set; }
+        private string Nome { get; set; }
+
+        private double Valor { get; set; }
+
+        private string Data { get; set; }
+
+    }
+}
